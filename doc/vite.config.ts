762f4bc2d80310973803ac78885d632a3cf87419
Rik <1439713475@qq.com>
2025-07-10 16:41:02 +0800

    fix: url

--- doc/vite.config.ts
+++ doc/vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
+  base: '/shadcn-plus/',
   plugins: [react()],
   build: {
     outDir: path.resolve(process.env.INIT_CWD || './', './dist/doc'),
